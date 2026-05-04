f006d1477d9d47ce58ce0f1c6e593d806765148c
albert revazov <trezeguet15@mail.ru>
2026-05-04 22:59:50 +0300

    beginning product section styles & design

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -5,3 +5,4 @@ import styles from './Header.module.scss'
 export const Header = () => {
   return <header className={styles.header}><img src='image/logo.jpeg' height={73} width={73} loading="lazy" alt='logo'/><Navigation /></header>
 }
+
--- src/sections/product/Product.tsx
+++ src/sections/product/Product.tsx
@@ -1,6 +1,14 @@
 import React from "react"
 import styles from './Product.module.scss'
+import { ProductData } from "../../mocks/blocks/Product"
 
 export const Product = () => {
-  return <section id='product' className={styles.product}>Product</section>
+  return (
+    <section id='product' aria-label="блок продукции" className={styles.product}>
+      {/* <h2 className={styles.product_title}>{ProductData.title}</h2> */}
+      <h3 className={styles.product_description}>{ProductData.description}</h3>
+      <ul className={styles.product_list}>{ProductData.list.map((item) => <li key={item.id} className={styles.product_item}>- {item.title}</li>)}</ul>
+      <strong className={styles.product_ps}> {ProductData.ps}</strong>
+    </section>
+  )
 }
