a5bd5fff6847f2e030baee7753185b2f6bfd809b
albert revazov <trezeguet15@mail.ru>
2026-05-05 19:11:44 +0300

    draft design for products

--- src/sections/product/Product.tsx
+++ src/sections/product/Product.tsx
@@ -5,10 +5,8 @@ import { ProductData } from "../../mocks/blocks/Product"
 export const Product = () => {
   return (
     <section id='product' aria-label="блок продукции" className={styles.product}>
-      {/* <h2 className={styles.product_title}>{ProductData.title}</h2> */}
-      <h3 className={styles.product_description}>{ProductData.description}</h3>
+      <h2 className={styles.product_title}>{ProductData.title}</h2>
       <ul className={styles.product_list}>{ProductData.list.map((item) => <li key={item.id} className={styles.product_item}>- {item.title}</li>)}</ul>
-      <strong className={styles.product_ps}> {ProductData.ps}</strong>
     </section>
   )
 }
