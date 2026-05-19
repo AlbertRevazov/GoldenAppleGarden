05c9106abad9b99184e4dc58a378c706bf195b20
albert revazov <trezeguet15@mail.ru>
2026-05-19 21:51:55 +0300

    integrated swiper, some styles for catalog

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -3,6 +3,6 @@ import { Navigation } from "../../components/Navigation/Navigation"
 import styles from './Header.module.scss'
 
 export const Header = () => {
-  return <header className={styles.header}><img src='image/logo.jpeg' height={73} width={73} loading="lazy" alt='logo'/><Navigation /></header>
+  return <header className={styles.header}><img src='image/logo.webp' height={100} width={100} loading="lazy" alt='logo'/><Navigation /></header>
 }
 
--- src/sections/catalog/Catalog.tsx
+++ src/sections/catalog/Catalog.tsx
@@ -1,17 +1,41 @@
 import { CatalogData } from '../../mocks/Catalog'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Parallax } from 'swiper/modules';
 import styles from './Catalog.module.scss'
+import 'swiper/css';
 
 export const Catalog = () => {
   return (
     <article className={styles.catalog}>
-      <ul className={styles.catalog_list}>{CatalogData.map((item) =>
-        <li className={styles.catalog_item}>
-          <h3 className={styles.item_title}>{item.title}</h3>
-          <h4 className={styles.item_description}>{item.description}</h4>
-          <img className={styles.item_image} src={item.image} alt='image for apple' loading='lazy' width={250} />
-          <button type='button' className={styles.button} disabled={!item.availability}>{item.availability ? <a className={styles.link} href='#contacts'>{item.price} - Заказать</a> : 'Нет в Наличии'}</button>
-        </li>)}
-      </ul>
-    </article>
+      <Swiper className={styles.catalog_list}
+        modules={[Autoplay, Parallax]}
+        autoplay
+        parallax
+        loop
+        spaceBetween={40}
+        slidesPerView={1}
+        centeredSlides={true}
+        speed={2700}
+        direction='vertical'
+      >
+        {CatalogData.map((item) => {
+          return <SwiperSlide className={styles.catalog_item} data-swiper-autoplay='3000' data-swiper-parallax-duration="600"
+          >
+            <header className={styles.catalog_header}>
+              <h3 className={styles.item_title} data-swiper-parallax="-550" data-swiper-parallax-duration="1000">{item.title}</h3>
+              <h4 className={styles.item_description} data-swiper-parallax="-650" data-swiper-parallax-duration="1100">{item.description}</h4>
+              <button type='button' className={styles.button} disabled={!item.availability} data-swiper-parallax="-850" data-swiper-parallax-duration="1250">
+                {item.availability ?
+                  <a className={styles.link} href='#contacts' >
+                    {item.price} - Заказать</a> : 'Нет в Наличии'}
+              </button>
+            </header>
+            <img className={styles.item_image} src={item.image} alt='image for apple' loading='lazy' width={250} data-swiper-parallax="-750" data-swiper-parallax-duration="1200" />
+
+          </SwiperSlide>
+        }
+        )}
+      </Swiper>
+    </article >
   )
 }
